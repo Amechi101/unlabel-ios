963ba2899bb6321c253773228132b784b44b0576
amechi101 <egbe@usc.edu>
2015-11-15 13:39:17 -0500

    obj-c

---
+++ Unlabel/UnlabelMain/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Unlabel
+//
+//  Created by Amechi Egbe on 11/15/15.
+//  Copyright © 2015 Unlabel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
+
---
+++ Unlabel/UnlabelMain/RootViewController.m
@@ -0,0 +1,19 @@
+//
+//  RootViewController.m
+//  Unlabel
+//
+//  Created by Amechi Egbe on 11/15/15.
+//  Copyright © 2015 Unlabel. All rights reserved.
+//  Base View
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+//Code here!
+
+@end
