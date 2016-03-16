e560813c44f2de36782c14d56ae9133e424c0813
Zaid Pathan <zaidkhanintel@gmail.com>
2016-03-17 01:57:55 +0530

    Code structured.

--- Unlabel/Unlabel/Controller/EntryVC/EntryVC.swift
+++ Unlabel/Unlabel/Controller/EntryVC/EntryVC.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class EntryVC: UIViewController {
 
+    
+    //
+    //MARK:- VC Lifecycle
+    //
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,57 +25,59 @@ class EntryVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func unwindToEntry(segue:UIStoryboardSegue){
-    
-    }
-    
-    @IBAction func IBActionContinueWithFB(sender: UIButton) {
-      handleFBLogin()
-    }
-
-    func handleFBLogin(){
-        UnlabelFBHelper.login(fromViewController: self, successBlock: { () -> () in
-            
-            self.setupCognitoForFB()
-            
-            self.dismissViewControllerAnimated(true, completion: { () -> Void in
-                let rootNavVC = self.storyboard!.instantiateViewControllerWithIdentifier(S_ID_NAV_CONTROLLER) as? UINavigationController
-                if let window = APP_DELEGATE.window {
-                    UIView.transitionWithView(APP_DELEGATE.window!, duration: 0.5, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
-                        window.rootViewController = rootNavVC
-                        }, completion: nil)
-                }
-            })
-            }) { (error:NSError?) -> () in
-                print(error)
-        }
-    }
-    
-    func setupCognitoForFB(){
-        AWSHelper.configureAWSWithFBToken()
-        AWSHelper.getCredentialsProvider().getIdentityId().continueWithBlock { (task: AWSTask!) -> AnyObject! in
-            
-            if (task.error != nil) {
-                print("Error: " + task.error!.localizedDescription)
-                
-            } else {
-                // the task result will contain the identity id
-                let cognitoId = task.result
-                print(cognitoId)
-            }
-            return nil
-        }
-    }
- 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+    //
+    //MARK:- IBAction Methods
+    //
+
+    extension EntryVC {
+        @IBAction func unwindToEntry(segue:UIStoryboardSegue){
+            
+        }
+        
+        @IBAction func IBActionContinueWithFB(sender: UIButton) {
+            handleFBLogin()
+        }
+    }
+
+    
+    //
+    //MARK:- Custom Methods
+    //
+
+    extension EntryVC {
+        func handleFBLogin(){
+            UnlabelFBHelper.login(fromViewController: self, successBlock: { () -> () in
+                
+                self.setupCognitoForFB()
+                
+                self.dismissViewControllerAnimated(true, completion: { () -> Void in
+                    let rootNavVC = self.storyboard!.instantiateViewControllerWithIdentifier(S_ID_NAV_CONTROLLER) as? UINavigationController
+                    if let window = APP_DELEGATE.window {
+                        UIView.transitionWithView(APP_DELEGATE.window!, duration: 0.5, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
+                            window.rootViewController = rootNavVC
+                            }, completion: nil)
+                    }
+                })
+                }) { (error:NSError?) -> () in
+                    print(error)
+            }
+        }
+        
+        func setupCognitoForFB(){
+            AWSHelper.configureAWSWithFBToken()
+            AWSHelper.getCredentialsProvider().getIdentityId().continueWithBlock { (task: AWSTask!) -> AnyObject! in
+                
+                if (task.error != nil) {
+                    print("Error: " + task.error!.localizedDescription)
+                    
+                } else {
+                    // the task result will contain the identity id
+                    let cognitoId = task.result
+                    print(cognitoId)
+                }
+                return nil
+            }
+        }
+    }
